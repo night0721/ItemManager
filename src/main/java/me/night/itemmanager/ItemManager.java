3e9631d62406886a01e1eb33917f5e7b3f4b72b1
night0721 <oscarfu0721@gmail.com>
2022-08-04 17:22:04 +0100

    Initial commit

---
+++ src/main/java/me/night/itemmanager/ItemManager.java
@@ -0,0 +1,17 @@
+package me.night.itemmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
